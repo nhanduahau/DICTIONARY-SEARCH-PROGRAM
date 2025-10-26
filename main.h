d2a2c3d6a33f3c85caa566d713eece6ed3c6689a
nhanduahau <tolyhuunhan@gmail.com>
2025-10-26 14:09:15 -0500

    Refactor main.cpp and add Main.h for improved organization and clarity

---
+++ main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include "Functions.h"
+
+using namespace std;
+
+// Default dictionary file used by main
+static constexpr const char *DICTIONARY_FILENAME = "dictionary.txt";
+
+#endif // MAIN_H
